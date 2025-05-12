54124554b9a9034c8a5ebf584fbb9133122244f5
Brandon Hancock <brandon@brandonhancock.io>
2025-05-12 14:30:32 -0400

    v1

---
+++ app/google_search_agent/agent.py
@@ -0,0 +1,16 @@
+from google.adk.agents import Agent
+from google.adk.tools import google_search  # Import the tool
+
+root_agent = Agent(
+    # A unique name for the agent.
+    name="basic_search_agent",
+    # The Large Language Model (LLM) that agent will use.
+    model="gemini-2.0-flash-exp",
+    # model="gemini-2.0-flash-live-001",  # New streaming model version as of Feb 2025
+    # A short description of the agent's purpose.
+    description="Agent to answer questions using Google Search.",
+    # Instructions to set the agent's behavior.
+    instruction="You are an expert researcher. You always stick to the facts. Make sure you answer the questions as concisely as possible. Answer the question and don't provide any additional information.",
+    # Add google_search tool to perform grounding with Google search.
+    tools=[google_search],
+)
